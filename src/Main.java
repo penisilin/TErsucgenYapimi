17802661b547e257752fd9443398919b9f7167aa
penisilin <112887815+penisilin@users.noreply.github.com>
2022-12-01 19:00:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.println("sayı giriniz:");
+        int n = inp.nextInt();
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int k = n; k > i; k--) {
+                System.out.print(" ");
+            }
+
+            for (int l = 1; l <= (2 * i - 1); l++) {
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
+
+        }
+
+
+    }
+}
